20ff42233352fd45f5eaad62c8cd9b81dcb9446f
Liam Thorell <liamthorell18@gmail.com>
2021-04-01 00:46:49 +0200

    First Changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Brazil from '../views/Brazil.vue'
+import Jamaica from '../views/Jamaica.vue'
+import Panama from '../views/Panama.vue'
+import Hawaii from '../views/Hawaii.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +21,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil,
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: Hawaii,
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: Panama,
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: Jamaica,
+  },
 ]
 
 const router = new VueRouter({
