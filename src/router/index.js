653748b85144559fb02874634008fe655e81fe1f
Liam Thorell <liamthorell18@gmail.com>
2021-04-01 17:13:06 +0200

    Fixed Nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Brazil from '../views/Brazil.vue'
-import Jamaica from '../views/Jamaica.vue'
-import Panama from '../views/Panama.vue'
-import Hawaii from '../views/Hawaii.vue'
 
 Vue.use(VueRouter)
 
@@ -17,30 +13,27 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: "/brazil",
     name: "brazil",
-    component: Brazil,
+    component: () => import('../views/About.vue')
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: Hawaii,
+    component: () => import('../views/Hawaii.vue')
   },
   {
     path: "/panama",
     name: "panama",
-    component: Panama,
+    component: () => import('../views/Panama.vue')
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: Jamaica,
+    component: () => import('../views/Jamaica.vue')
   },
 ]
 
